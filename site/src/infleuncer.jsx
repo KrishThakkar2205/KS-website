7237f76cc6027e731f16420f3e1f927be6a1c4e5
KrishThakkar2205 <kdthakkar22@gmail.com>
2025-02-07 11:11:48 +0530

    adding Influencer

--- site/src/infleuncer.jsx
+++ site/src/infleuncer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import './App.css'
 
 function Infleuncer () {
     return (
@@ -7,14 +8,14 @@ function Infleuncer () {
             <Container fluid className="bg-dark">
                 <Card className="shadow-sm" bg="secondary">
                 <Row className="g-0">
-                    <Col md={4}>
-                        <Card.Img src="https://via.placeholder.com/150" alt="Infleuncer Image From Instsgram" className="h-100" />
+                    <Col xs={4} md={2} >
+                        <Card.Img src="https://storage.googleapis.com/ares-profile-pictures/hd/millicentrose-71e7f4e3d3866b6399ccf7a226d4d766_hd.jpg" alt="Infleuncer Image From Instsgram" className="mini-profile-img"/>
                     </Col>
-                    <Col md={8}>
+                    <Col xs={8} md={10}>
                         <Card.Body>
                             <Card.Title>Infleuncer Name</Card.Title>
                             <Card.Text>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quibusdam obcaecati laudantium magni rerum quam eaque unde iusto? Minima voluptates alias omnis atque voluptas, assumenda labore explicabo aspernatur facilis. Placeat!
+                                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quibusdam obcaecati laudantium magni rerum quam eaque unde iusto? Minima voluptates alias omnis atque voluptas, assumenda labore explicabo aspernatur facilis. Placeat! */}
                             </Card.Text>
                             <Button variant="outline-warning">View Profile</Button>
                         </Card.Body>
