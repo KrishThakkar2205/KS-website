ae63db67672260feb791a5ed385cdc14cad2d19f
KrishThakkar2205 <kdthakkar22@gmail.com>
2025-02-07 22:06:17 +0530

    infleuncer file

--- frontend/src/components/Influencer.jsx
+++ frontend/src/components/Influencer.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import axios from 'axios';
+
+import React, { useEffect, useState } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import '../index.css'
 
 function Infleuncer () {
+    
+    const [influencers, getInfluencers] = useState([])
+
+    useEffect(()=>{ 
+        const getData = async() => {
+            try {
+                const data = await axios.get('https://67a5bd10c0ac39787a1f3d8d.mockapi.io/influencer/user')
+                getInfluencers(data.data);
+            } catch (error) {
+                console.log('some error occour ');
+                console.log(error);
+            }
+        }
+        getData();
+    },[]);
+
+
     return (
         <>
-            <Container fluid className="bg-dark">
-                <Card className="shadow-sm" bg="secondary">
-                <Row className="g-0">
-                    <Col xs={4} md={2} >
-                        <Card.Img src="https://storage.googleapis.com/ares-profile-pictures/hd/millicentrose-71e7f4e3d3866b6399ccf7a226d4d766_hd.jpg" alt="Infleuncer Image From Instsgram" className="mini-profile-img"/>
-                    </Col>
-                    <Col xs={8} md={10}>
+            
+            <Container fluid className="bg-dark h-100">
+                <Row>
+                {influencers.map((data) => (         
+                
+                    <Col md={3} xs={6}>
+                        <Card className="shadow-sm mt-3" bg="secondary">
+                        <Card.Img src={data.image} alt="Infleuncer Image From Instsgram"/>
                         <Card.Body>
-                            <Card.Title>Infleuncer Name</Card.Title>
+                            <Card.Title>{data.name}</Card.Title>
                             <Card.Text>
-                                {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quibusdam obcaecati laudantium magni rerum quam eaque unde iusto? Minima voluptates alias omnis atque voluptas, assumenda labore explicabo aspernatur facilis. Placeat! */}
+                            {data.gender}
                             </Card.Text>
                             <Button variant="outline-warning">View Profile</Button>
                         </Card.Body>
+                        </Card>
                     </Col>
-                </Row>
-                </Card>
+                ))}
+            </Row>
             </Container>
         </>
     )
